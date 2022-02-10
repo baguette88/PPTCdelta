0cdee451bdf368ab69ab41e50ab40bf9569aa4df
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2022-02-10 15:48:17 -0500

    roadmapx
    x

--- src/App.js
+++ src/App.js
@@ -444,13 +444,13 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/roadmap.png" : null}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         <StyledLogo alt={"logo"} src={"/config/images/petrentals.png"} />
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/petrentals.png"} />
+            <StyledImg alt={"example"} src={"/config/images/roadmap.png"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
